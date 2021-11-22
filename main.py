2934fa56f5c12b25a1b3fa63b5d26be6052c0b61
Jonathan Garza <83787061+jonathanrgarza@users.noreply.github.com>
2021-11-21 18:21:28 -0600

    Add initial project files

---
+++ main.py
@@ -0,0 +1,37 @@
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+def run_cartpole():
+    # Parallel Environments
+    env = make_vec_env("CartPole-v1", n_envs=2)
+
+    model = PPO("MlpPolicy", env, learning_rate=0.1, gamma=0.6)
+    model.learn(total_timesteps=25000)
+    model.save("models/ppo_cartpole")
+
+    del model
+    del env
+
+    model = PPO.load("models/ppo_cartpole")
+
+    env = gym.make("CartPole-v1")
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        env.render()
+
+        if done:
+            break
+
+    env.close()
+
+
+def main():
+    run_cartpole()
+
+
+if __name__ == "__main__":
+    main()
+    print("Finished program")
